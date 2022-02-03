1dcb61e3530035cd0102001fd247a52ffe6e4343
sl0ck <rastasonna@gmail.com>
2022-02-03 12:29:00 +0700

    First Release

---
+++ members.js
@@ -0,0 +1,74 @@
+members = [
+    {
+        "name": "Drepram",
+        "stats": {
+            "popl": 37.9,
+            "peri": 67.9,
+            "comp": 90,
+            "inov": 20.8
+        }
+    },
+	{
+        "name": "Deby",
+        "stats": {
+            "popl": 15,
+            "peri": 0,
+            "comp": 91.7,
+            "inov": 14.3
+        }
+    },
+	{
+        "name": "Indira",
+        "stats": {
+            "popl": 33.3,
+            "peri": 8.8,
+            "comp": 65,
+            "inov": 33.3
+        }
+    },
+	{
+        "name": "Ferdinand",
+        "stats": {
+            "popl": 9.1,
+            "peri": 0,
+            "comp": 75,
+            "inov": 92.3
+        }
+    },
+	{
+        "name": "Razita",
+        "stats": {
+            "popl": 31.3,
+            "peri": 3.3,
+            "comp": 94.4,
+            "inov": 17.4
+        }
+    },
+	{
+        "name": "Yiid",
+        "stats": {
+            "popl": 63.3,
+            "peri": 0,
+            "comp": 25,
+            "inov": 100
+        }
+    },
+	{
+        "name": "Faris",
+        "stats": {
+            "popl": 52.6,
+            "peri": 84.4,
+            "comp": 100,
+            "inov": 96.6
+        }
+    },
+	{
+        "name": "Jepri",
+        "stats": {
+            "popl": 61.3,
+            "peri": 51.7,
+            "comp": 100,
+            "inov": 31.3
+        }
+    },
+];
